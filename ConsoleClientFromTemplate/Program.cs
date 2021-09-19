0eb77b8c2664cf9c5cdd561e9d4e96ebd6fe8f40
Wojciech Swieboda <wswieboda@chathamfinancial.com>
2021-09-19 16:26:29 +0200

    - projects created from Template - service reference created - NodaTime package added

---
+++ ConsoleClientFromTemplate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleClientFromTemplate
+{
+    static class Program
+    {
+        static async Task Main()
+        {
+            var httpClient = new HttpClient();
+            var client = new swaggerClient("https://localhost:5001/", httpClient);
+            var results = (await client.WeatherForecastAsync());
+            var firstForecast = results.First();
+            Console.WriteLine(JsonSerializer.Serialize(firstForecast, new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            }));
+        }
+    }
+}
