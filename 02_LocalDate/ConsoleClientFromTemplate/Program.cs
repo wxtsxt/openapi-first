d759009b51e9ad94279f1c523a1d8e8eae97d519
Wojciech Swieboda <wswieboda@chathamfinancial.com>
2021-09-28 17:50:52 +0200

    02 expose and consume NodaTime.LocalDate

---
+++ 02_LocalDate/ConsoleClientFromTemplate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using NodaTime;
+using NodaTime.Serialization.SystemTextJson;
+
+namespace ConsoleClientFromTemplate
+{
+    static class Program
+    {
+        static async Task Main()
+        {
+            var httpClient = new HttpClient();
+            var client = new swaggerClient("https://localhost:5001/", httpClient);
+            var results = await client.WeatherForecastAsync();
+            WeatherForecast firstForecast = results.First();
+            Console.WriteLine(JsonSerializer.Serialize(firstForecast, new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            }));
+        }
+    }
+
+    public partial class swaggerClient
+    {
+        partial void UpdateJsonSerializerSettings(Newtonsoft.Json.JsonSerializerSettings settings)
+        {
+            // we may adjust JsonSerializerSettings, but NodaTime.Serialization.SystemTextJson package supports only adjusting JsonSerializerOptions
+        }
+
+    }
+}
---
+++ 02_LocalDate/WebApiFromTemplate/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NodaTime;
+
+namespace WebApiFromTemplate.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index =>
+                {
+                    var now = DateTime.Now.AddDays(index);
+
+                    return new WeatherForecast
+                    {
+                        Date = new LocalDate(now.Year, now.Month, now.Day),
+                        TemperatureC = rng.Next(-20, 55),
+                        Summary = Summaries[rng.Next(Summaries.Length)]
+                    };
+                })
+            .ToArray();
+        }
+    }
+}
