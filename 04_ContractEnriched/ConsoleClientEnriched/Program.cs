3965714920ced143b792f2d0c07bfd2dc5ec8e98
Wojciech Swieboda <wswieboda@chathamfinancial.com>
2021-10-03 20:14:36 +0200

    - sample 4

---
+++ 04_ContractEnriched/ConsoleClientEnriched/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NodaTime;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApiFromTemplate;
+
+namespace ConsoleClientEnriched
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var client = new contract_first_swaggerClient("https://localhost:5041/", httpClient);
+            var response = await client.WeatherForecastAsync(new WeatherForecast
+            {
+                DateOf = new LocalDate(2020,1,31),
+                Summary = "cold",
+                TemperatureC = 0
+            });
+            Console.WriteLine(response.DateOf);
+        }
+    }
+}
